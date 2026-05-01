f7135eb8412a5a461e4c045c0efe368b5c2e2ba2
Kristofer <kris@youngers.org>
2026-05-01 09:15:09 -0400

    keep category changes

--- src/main/java/rocks/zipcode/blozzer/entity/Category.java
+++ src/main/java/rocks/zipcode/blozzer/entity/Category.java
@@ -18,13 +18,13 @@ public class Category {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer categoryId;
-	@Column(name = "title", length = 100, nullable =false) 
+	@Column(name = "title", length = 100, nullable = false)
 	private String categoryTitle;
 	@Column(name = "description")
 	private String categoryDescription;
-	
-	@OneToMany(mappedBy = "category" , cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-	private List<Post> posts = new ArrayList<>(); 
+
+	@OneToMany(mappedBy = "category", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private List<Post> posts = new ArrayList<>();
 
 	public Category() {
 	}
